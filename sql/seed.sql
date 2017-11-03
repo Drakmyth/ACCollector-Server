1374167e8ac0f450e95b5b2dd2089402ed7cd118
Drakmyth <shaun.hamman@gmail.com>
2017-11-02 17:37:59 -0700

    Added seed script. Modified setup script to not change context.

---
+++ sql/seed.sql
@@ -0,0 +1,71 @@
+﻿DELETE FROM [ACCollector].[dbo].[Release];
+DELETE FROM [ACCollector].[dbo].[Game];
+
+-- Animal Forest
+DECLARE @Id UNIQUEIDENTIFIER;
+INSERT INTO [ACCollector].[dbo].[Game](Name) VALUES ('Animal Forest');
+SET @Id = (SELECT GameId from [ACCollector].[dbo].[Game] Where Name = 'Animal Forest');
+INSERT INTO [ACCollector].[dbo].[Release](GameId, RegionCode, Title, PlatformCode, ReleaseDate) VALUES
+(@Id, 'JP', 'Dōbutsu no Mori', 'N64', '2001-04-14');
+GO
+
+-- Animal Forest+
+DECLARE @Id UNIQUEIDENTIFIER;
+INSERT INTO [ACCollector].[dbo].[Game](Name) VALUES ('Animal Forest+');
+SET @Id = (SELECT GameId from [ACCollector].[dbo].[Game] Where Name = 'Animal Forest+');
+INSERT INTO [ACCollector].[dbo].[Release](GameId, RegionCode, Title, PlatformCode, ReleaseDate) VALUES
+(@Id, 'JP', 'Dōbutsu no Mori+', 'GCN', '2001-12-14'),
+(@Id, 'CHN', 'Dòngwù Sēnlín', 'iQue', '2006-06-01');
+GO
+
+-- Animal Crossing
+DECLARE @Id UNIQUEIDENTIFIER;
+INSERT INTO [ACCollector].[dbo].[Game](Name) VALUES ('Animal Crossing');
+SET @Id = (SELECT GameId from [ACCollector].[dbo].[Game] Where Name = 'Animal Crossing');
+INSERT INTO [ACCollector].[dbo].[Release](GameId, RegionCode, Title, PlatformCode, ReleaseDate) VALUES
+(@Id, 'NA', 'Animal Crossing', 'GCN', '2002-09-15'),
+(@Id, 'AU', 'Animal Crossing', 'GCN', '2003-10-17'),
+(@Id, 'EU', 'Animal Crossing', 'GCN', '2004-09-24');
+GO
+
+-- Animal Forest e+
+DECLARE @Id UNIQUEIDENTIFIER;
+INSERT INTO [ACCollector].[dbo].[Game](Name) VALUES ('Animal Forest e+');
+SET @Id = (SELECT GameId from [ACCollector].[dbo].[Game] Where Name = 'Animal Forest e+');
+INSERT INTO [ACCollector].[dbo].[Release](GameId, RegionCode, Title, PlatformCode, ReleaseDate) VALUES
+(@Id, 'JP', 'Dōbutsu no Mori e+', 'GCN', '2003-06-27');
+GO
+
+-- Animal Crossing: Wild World
+DECLARE @Id UNIQUEIDENTIFIER;
+INSERT INTO [ACCollector].[dbo].[Game](Name) VALUES ('Wild World');
+SET @Id = (SELECT GameId from [ACCollector].[dbo].[Game] Where Name = 'Wild World');
+INSERT INTO [ACCollector].[dbo].[Release](GameId, RegionCode, Title, PlatformCode, ReleaseDate) VALUES
+(@Id, 'JP', 'Oideyo Dōbutsu no Mori', 'NDS', '2005-11-23'),
+(@Id, 'NA', 'Animal Crossing: Wild World', 'NDS', '2005-12-05'),
+(@Id, 'AU', 'Animal Crossing: Wild World', 'NDS', '2005-12-08'),
+(@Id, 'EU', 'Animal Crossing: Wild World', 'NDS', '2006-03-31');
+GO
+
+-- Animal Crossing: City Folk
+DECLARE @Id UNIQUEIDENTIFIER;
+INSERT INTO [ACCollector].[dbo].[Game](Name) VALUES ('City Folk');
+SET @Id = (SELECT GameId from [ACCollector].[dbo].[Game] Where Name = 'City Folk');
+INSERT INTO [ACCollector].[dbo].[Release](GameId, RegionCode, Title, PlatformCode, ReleaseDate) VALUES
+(@Id, 'JP', 'Machi e ikō yo: Dōbutsu no Mori', 'Wii', '2008-11-20'),
+(@Id, 'NA', 'Animal Crossing: City Folk', 'Wii', '2008-11-16'),
+(@Id, 'AU', 'Animal Crossing: Let''s Go to the City', 'Wii', '2008-11-04'),
+(@Id, 'EU', 'Animal Crossing: Let''s Go to the City', 'Wii', '2008-11-05');
+GO
+
+-- Animal Crossing: New Leaf
+DECLARE @Id UNIQUEIDENTIFIER;
+INSERT INTO [ACCollector].[dbo].[Game](Name) VALUES ('New Leaf');
+SET @Id = (SELECT GameId from [ACCollector].[dbo].[Game] Where Name = 'New Leaf');
+INSERT INTO [ACCollector].[dbo].[Release](GameId, RegionCode, Title, PlatformCode, ReleaseDate) VALUES
+(@Id, 'JP', 'Tobidase Dōbutsu no Mori', '3DS', '2012-11-08'),
+(@Id, 'KOR', 'Animal Crossing: New Leaf', '3DS', '2013-02-07'),
+(@Id, 'NA', 'Animal Crossing: New Leaf', '3DS', '2013-06-09'),
+(@Id, 'AU', 'Animal Crossing: New Leaf', '3DS', '2013-06-15'),
+(@Id, 'EU', 'Animal Crossing: New Leaf', '3DS', '2013-06-14');
+GO
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,33 +1,32 @@
-﻿USE master;
+﻿USE [master];
 GO
 
-CREATE DATABASE ACCollector
+IF EXISTS (select * from INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'ACCollector')
+BEGIN
+	ALTER DATABASE [ACCollector] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+END
+GO
+DROP DATABASE IF EXISTS [ACCollector];
 GO
 
-USE [ACCollector]
+CREATE DATABASE [ACCollector]
 GO
 
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[Game] (
+CREATE TABLE [ACCollector].[dbo].[Game] (
     [GameId] UNIQUEIDENTIFIER NOT NULL CONSTRAINT DF_Game_GameId DEFAULT NEWSEQUENTIALID(),
     [Name] VARCHAR (60) NOT NULL,
 	CONSTRAINT PK_Game PRIMARY KEY CLUSTERED (GameId)
 );
 GO
 
-CREATE TABLE [dbo].[Region] (
+CREATE TABLE [ACCollector].[dbo].[Region] (
 	[RegionCode] VARCHAR(3) NOT NULL,
 	[Description] VARCHAR(20) NOT NULL,
 	CONSTRAINT PK_Region PRIMARY KEY CLUSTERED (RegionCode)
 );
 GO
 
-INSERT INTO [dbo].[Region](RegionCode, Description) VALUES
+INSERT INTO [ACCollector].[dbo].[Region](RegionCode, Description) VALUES
 ('JP', 'Japan'),
 ('NA', 'North America'),
 ('EU', 'Europe'),
@@ -36,14 +35,14 @@ INSERT INTO [dbo].[Region](RegionCode, Description) VALUES
 ('CHN', 'China');
 GO
 
-CREATE TABLE [dbo].[Platform] (
+CREATE TABLE [ACCollector].[dbo].[Platform] (
 	[PlatformCode] VARCHAR(4) NOT NULL,
 	[Description] VARCHAR(40) NOT NULL,
 	CONSTRAINT PK_Platform PRIMARY KEY CLUSTERED (PlatformCode)
 );
 GO
 
-INSERT INTO [dbo].[Platform](PlatformCode, Description) VALUES
+INSERT INTO [ACCollector].[dbo].[Platform](PlatformCode, Description) VALUES
 ('N64', 'Nintendo 64'),
 ('GCN', 'Nintendo Gamecube'),
 ('iQue', 'iQue Player'),
@@ -52,25 +51,25 @@ INSERT INTO [dbo].[Platform](PlatformCode, Description) VALUES
 ('3DS', 'Nintendo 3DS');
 GO
 
-CREATE TABLE [dbo].[IslandStatus] (
+CREATE TABLE [ACCollector].[dbo].[IslandStatus] (
 	IslandStatus VARCHAR(10) NOT NULL
 	CONSTRAINT PK_IslandStatus PRIMARY KEY CLUSTERED (IslandStatus)
 );
 GO
 
-INSERT INTO [dbo].[IslandStatus](IslandStatus) VALUES
+INSERT INTO [ACCollector].[dbo].[IslandStatus](IslandStatus) VALUES
 ('None'),
 ('Available'),
 ('Exclusive');
 GO
 
-CREATE TABLE [dbo].[Size] (
+CREATE TABLE [ACCollector].[dbo].[Size] (
 	Size VARCHAR(10) NOT NULL
 	CONSTRAINT PK_Size PRIMARY KEY CLUSTERED (Size)
 );
 GO
 
-INSERT INTO [dbo].[Size](Size) VALUES
+INSERT INTO [ACCollector].[dbo].[Size](Size) VALUES
 ('Tiny'),
 ('Small'),
 ('Medium'),
@@ -81,7 +80,7 @@ INSERT INTO [dbo].[Size](Size) VALUES
 ('Fin');
 GO
 
-CREATE TABLE [dbo].[Release] (
+CREATE TABLE [ACCollector].[dbo].[Release] (
     [ReleaseId] UNIQUEIDENTIFIER NOT NULL CONSTRAINT DF_Release_ReleaseId DEFAULT NEWSEQUENTIALID(),
     [GameId] UNIQUEIDENTIFIER NOT NULL,
 	[RegionCode] VARCHAR(3) NOT NULL,
@@ -89,26 +88,26 @@ CREATE TABLE [dbo].[Release] (
 	[PlatformCode] VARCHAR(4) NOT NULL,
 	[ReleaseDate] DATETIME2 NOT NULL
 	CONSTRAINT PK_Release PRIMARY KEY CLUSTERED (ReleaseId),
-	CONSTRAINT FK_Release_Game_GameId FOREIGN KEY (GameId) REFERENCES [dbo].[Game](GameId) ON DELETE CASCADE,
-	CONSTRAINT FK_Release_Region_RegionCode FOREIGN KEY (RegionCode) REFERENCES [dbo].[Region](RegionCode),
-	CONSTRAINT FK_Release_Platform_PlatformCode FOREIGN KEY (PlatformCode) REFERENCES [dbo].[Platform](PlatformCode)
+	CONSTRAINT FK_Release_Game_GameId FOREIGN KEY (GameId) REFERENCES [ACCollector].[dbo].[Game](GameId) ON DELETE CASCADE,
+	CONSTRAINT FK_Release_Region_RegionCode FOREIGN KEY (RegionCode) REFERENCES [ACCollector].[dbo].[Region](RegionCode),
+	CONSTRAINT FK_Release_Platform_PlatformCode FOREIGN KEY (PlatformCode) REFERENCES [ACCollector].[dbo].[Platform](PlatformCode)
 );
 GO
 
-CREATE TABLE [dbo].[Bug] (
+CREATE TABLE [ACCollector].[dbo].[Bug] (
 	[BugId] UNIQUEIDENTIFIER NOT NULL CONSTRAINT DF_Bug_BugId DEFAULT NEWSEQUENTIALID(),
 	[GameId] UNIQUEIDENTIFIER NOT NULL,
 	[SalePrice] INT NOT NULL,
 	[Location] VARCHAR(10) NOT NULL,
 	[IslandStatus] VARCHAR(10) NOT NULL,
 	CONSTRAINT PK_Bug PRIMARY KEY CLUSTERED (BugId),
-	CONSTRAINT FK_Bug_Game_GameId FOREIGN KEY (GameId) REFERENCES [dbo].[Game](GameId) ON DELETE CASCADE,
-	CONSTRAINT FK_Bug_IslandStatus_IslandStatus FOREIGN KEY (IslandStatus) REFERENCES [dbo].[IslandStatus](IslandStatus),
+	CONSTRAINT FK_Bug_Game_GameId FOREIGN KEY (GameId) REFERENCES [ACCollector].[dbo].[Game](GameId) ON DELETE CASCADE,
+	CONSTRAINT FK_Bug_IslandStatus_IslandStatus FOREIGN KEY (IslandStatus) REFERENCES [ACCollector].[dbo].[IslandStatus](IslandStatus),
 	CONSTRAINT CHK_Bug_SalePrice CHECK (SalePrice >= 0)
 );
 GO
 
-CREATE TABLE [dbo].[BugAvailability] (
+CREATE TABLE [ACCollector].[dbo].[BugAvailability] (
 	[BugId] UNIQUEIDENTIFIER NOT NULL,
 	[StartMonth] INT NOT NULL,
 	[StartHour] INT NOT NULL,
@@ -117,7 +116,7 @@ CREATE TABLE [dbo].[BugAvailability] (
 	[StartsMidMonth] BIT NOT NULL CONSTRAINT DF_BugAvailability_StartsMidMonth DEFAULT 0,
 	[EndsMidMonth] BIT NOT NULL CONSTRAINT DF_BugAvailability_EndsMidMonth DEFAULT 0
 	CONSTRAINT PK_BugAvailability PRIMARY KEY CLUSTERED (BugId)
-	CONSTRAINT FK_BugAvailability_Bug_BugId FOREIGN KEY (BugId) REFERENCES [dbo].[Bug](BugId) ON DELETE CASCADE,
+	CONSTRAINT FK_BugAvailability_Bug_BugId FOREIGN KEY (BugId) REFERENCES [ACCollector].[dbo].[Bug](BugId) ON DELETE CASCADE,
 	CONSTRAINT CHK_BugAvailability_StartMonth CHECK (StartMonth >= 1 AND StartMonth <= 12),
 	CONSTRAINT CHK_BugAvailability_StartHour CHECK (StartHour >= 0 AND StartHour <= 23),
 	CONSTRAINT CHK_BugAvailability_EndMonth CHECK (EndMonth >= 1 AND EndMonth <= 12),
@@ -125,7 +124,7 @@ CREATE TABLE [dbo].[BugAvailability] (
 );
 GO
 
-CREATE TABLE [dbo].[Fish] (
+CREATE TABLE [ACCollector].[dbo].[Fish] (
 	[FishId] UNIQUEIDENTIFIER NOT NULL CONSTRAINT DF_Fish_FishId DEFAULT NEWSEQUENTIALID(),
 	[GameId] UNIQUEIDENTIFIER NOT NULL,
 	[SalePrice] INT NOT NULL,
@@ -133,14 +132,14 @@ CREATE TABLE [dbo].[Fish] (
 	[Location] VARCHAR(10) NOT NULL,
 	[IslandStatus] VARCHAR(10) NOT NULL,
 	CONSTRAINT PK_Fish PRIMARY KEY CLUSTERED (FishId),
-	CONSTRAINT FK_Fish_Game_GameId FOREIGN KEY (GameId) REFERENCES [dbo].[Game](GameId) ON DELETE CASCADE,
-	CONSTRAINT FK_Fish_Size_Size FOREIGN KEY (Size) REFERENCES [dbo].[Size](Size),
-	CONSTRAINT FK_Fish_IslandStatus_IslandStatus FOREIGN KEY (IslandStatus) REFERENCES [dbo].[IslandStatus](IslandStatus),
+	CONSTRAINT FK_Fish_Game_GameId FOREIGN KEY (GameId) REFERENCES [ACCollector].[dbo].[Game](GameId) ON DELETE CASCADE,
+	CONSTRAINT FK_Fish_Size_Size FOREIGN KEY (Size) REFERENCES [ACCollector].[dbo].[Size](Size),
+	CONSTRAINT FK_Fish_IslandStatus_IslandStatus FOREIGN KEY (IslandStatus) REFERENCES [ACCollector].[dbo].[IslandStatus](IslandStatus),
 	CONSTRAINT CHK_Fish_SalePrice CHECK (SalePrice >= 0)
 );
 GO
 
-CREATE TABLE [dbo].[FishAvailability] (
+CREATE TABLE [ACCollector].[dbo].[FishAvailability] (
 	[FishId] UNIQUEIDENTIFIER NOT NULL,
 	[StartMonth] INT NOT NULL,
 	[StartHour] INT NOT NULL,
@@ -149,7 +148,7 @@ CREATE TABLE [dbo].[FishAvailability] (
 	[StartsMidMonth] BIT NOT NULL CONSTRAINT DF_FishAvailability_StartsMidMonth DEFAULT 0,
 	[EndsMidMonth] BIT NOT NULL CONSTRAINT DF_FishAvailability_EndsMidMonth DEFAULT 0
 	CONSTRAINT PK_FishAvailability PRIMARY KEY CLUSTERED (FishId)
-	CONSTRAINT FK_FishAvailability_Fish_FishId FOREIGN KEY (FishId) REFERENCES [dbo].[Fish](FishId) ON DELETE CASCADE,
+	CONSTRAINT FK_FishAvailability_Fish_FishId FOREIGN KEY (FishId) REFERENCES [ACCollector].[dbo].[Fish](FishId) ON DELETE CASCADE,
 	CONSTRAINT CHK_FishAvailability_StartMonth CHECK (StartMonth >= 1 AND StartMonth <= 12),
 	CONSTRAINT CHK_FishAvailability_StartHour CHECK (StartHour >= 0 AND StartHour <= 23),
 	CONSTRAINT CHK_FishAvailability_EndMonth CHECK (EndMonth >= 1 AND EndMonth <= 12),
