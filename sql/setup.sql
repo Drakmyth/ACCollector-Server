27280397ba41f075db7b91e04c461b8f7c8c54a6
Drakmyth <shaun.hamman@gmail.com>
2017-11-07 12:37:29 -0800

    Added Art and Fossil related tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -15,18 +15,18 @@ GO
 CREATE TABLE [ACCollector].[dbo].[Game] (
     [GameId] UNIQUEIDENTIFIER NOT NULL CONSTRAINT DF_Game_GameId DEFAULT NEWSEQUENTIALID(),
     [Name] VARCHAR (60) NOT NULL,
-	CONSTRAINT PK_Game PRIMARY KEY CLUSTERED (GameId)
+	CONSTRAINT PK_Game PRIMARY KEY CLUSTERED ([GameId])
 );
 GO
 
 CREATE TABLE [ACCollector].[dbo].[Region] (
 	[RegionCode] VARCHAR(3) NOT NULL,
 	[Description] VARCHAR(20) NOT NULL,
-	CONSTRAINT PK_Region PRIMARY KEY CLUSTERED (RegionCode)
+	CONSTRAINT PK_Region PRIMARY KEY CLUSTERED ([RegionCode])
 );
 GO
 
-INSERT INTO [ACCollector].[dbo].[Region](RegionCode, Description) VALUES
+INSERT INTO [ACCollector].[dbo].[Region]([RegionCode], [Description]) VALUES
 ('JP', 'Japan'),
 ('NA', 'North America'),
 ('EU', 'Europe'),
@@ -38,11 +38,11 @@ GO
 CREATE TABLE [ACCollector].[dbo].[Platform] (
 	[PlatformCode] VARCHAR(4) NOT NULL,
 	[Description] VARCHAR(40) NOT NULL,
-	CONSTRAINT PK_Platform PRIMARY KEY CLUSTERED (PlatformCode)
+	CONSTRAINT PK_Platform PRIMARY KEY CLUSTERED ([PlatformCode])
 );
 GO
 
-INSERT INTO [ACCollector].[dbo].[Platform](PlatformCode, Description) VALUES
+INSERT INTO [ACCollector].[dbo].[Platform]([PlatformCode], [Description]) VALUES
 ('N64', 'Nintendo 64'),
 ('GCN', 'Nintendo Gamecube'),
 ('iQue', 'iQue Player'),
@@ -53,11 +53,11 @@ GO
 
 CREATE TABLE [ACCollector].[dbo].[IslandStatus] (
 	[IslandStatus] VARCHAR(10) NOT NULL
-	CONSTRAINT PK_IslandStatus PRIMARY KEY CLUSTERED (IslandStatus)
+	CONSTRAINT PK_IslandStatus PRIMARY KEY CLUSTERED ([IslandStatus])
 );
 GO
 
-INSERT INTO [ACCollector].[dbo].[IslandStatus](IslandStatus) VALUES
+INSERT INTO [ACCollector].[dbo].[IslandStatus]([IslandStatus]) VALUES
 ('None'),
 ('Available'),
 ('Exclusive');
@@ -65,11 +65,11 @@ GO
 
 CREATE TABLE [ACCollector].[dbo].[FishSize] (
 	[Size] VARCHAR(10) NOT NULL
-	CONSTRAINT PK_Size PRIMARY KEY CLUSTERED (Size)
+	CONSTRAINT PK_FishSize PRIMARY KEY CLUSTERED ([Size])
 );
 GO
 
-INSERT INTO [ACCollector].[dbo].[FishSize](Size) VALUES
+INSERT INTO [ACCollector].[dbo].[FishSize]([Size]) VALUES
 ('Tiny'),
 ('Small'),
 ('Medium'),
@@ -80,6 +80,59 @@ INSERT INTO [ACCollector].[dbo].[FishSize](Size) VALUES
 ('Fin');
 GO
 
+CREATE TABLE [ACCollector].[dbo].[ArtType] (
+	[Type] VARCHAR(10) NOT NULL
+	CONSTRAINT PK_ArtType PRIMARY KEY CLUSTERED ([Type])
+);
+GO
+
+INSERT INTO [ACCollector].[dbo].[ArtType]([Type]) VALUES
+('Painting'),
+('Sculpture');
+GO
+
+CREATE TABLE [ACCollector].[dbo].[ArtSource] (
+	[Source] VARCHAR(10) NOT NULL
+	CONSTRAINT PK_ArtSource PRIMARY KEY CLUSTERED ([Source])
+);
+GO
+
+INSERT INTO [ACCollector].[dbo].[ArtSource]([Source]) VALUES
+('Crazy Redd'),
+('Tom Nook'),
+('Spotlight');
+GO
+
+CREATE TABLE [ACCollector].[dbo].[FossilGroup] (
+	[Group] VARCHAR(20) NOT NULL
+	CONSTRAINT PK_FossilGroup PRIMARY KEY CLUSTERED ([Group])
+);
+GO
+
+INSERT INTO [ACCollector].[dbo].[FossilGroup]([Group]) VALUES
+('Ankylosaurus'),
+('Apatosaurus'),
+('Archelon'),
+('Dimetrodon'),
+('Diplodocus'),
+('Ichthyosaur'),
+('Iguanodon'),
+('Mammoth'),
+('Megacerops'),
+('Pachycephalosaurus'),
+('Plesiosaur'),
+('Pteranodon'),
+('Sabretooth Tiger'),
+('Seismosaur'),
+('Stegosaur'),
+('Spinosaurus'),
+('Styracosaurus'),
+('Triceratops'),
+('Tyrannosaurus Rex'),
+('Velociraptor'),
+('Standalone');
+GO
+
 CREATE TABLE [ACCollector].[dbo].[Release] (
     [ReleaseId] UNIQUEIDENTIFIER NOT NULL CONSTRAINT DF_Release_ReleaseId DEFAULT NEWSEQUENTIALID(),
     [GameId] UNIQUEIDENTIFIER NOT NULL,
@@ -87,20 +140,20 @@ CREATE TABLE [ACCollector].[dbo].[Release] (
 	[Title] NVARCHAR(60) NOT NULL,
 	[PlatformCode] VARCHAR(4) NOT NULL,
 	[ReleaseDate] DATETIME2 NOT NULL
-	CONSTRAINT PK_Release PRIMARY KEY CLUSTERED (ReleaseId),
-	CONSTRAINT FK_Release_Game_GameId FOREIGN KEY (GameId) REFERENCES [ACCollector].[dbo].[Game](GameId) ON DELETE CASCADE,
-	CONSTRAINT FK_Release_Region_RegionCode FOREIGN KEY (RegionCode) REFERENCES [ACCollector].[dbo].[Region](RegionCode),
-	CONSTRAINT FK_Release_Platform_PlatformCode FOREIGN KEY (PlatformCode) REFERENCES [ACCollector].[dbo].[Platform](PlatformCode)
+	CONSTRAINT PK_Release PRIMARY KEY CLUSTERED ([ReleaseId]),
+	CONSTRAINT FK_Release_Game_GameId FOREIGN KEY ([GameId]) REFERENCES [ACCollector].[dbo].[Game]([GameId]) ON DELETE CASCADE,
+	CONSTRAINT FK_Release_Region_RegionCode FOREIGN KEY ([RegionCode]) REFERENCES [ACCollector].[dbo].[Region]([RegionCode]),
+	CONSTRAINT FK_Release_Platform_PlatformCode FOREIGN KEY ([PlatformCode]) REFERENCES [ACCollector].[dbo].[Platform]([PlatformCode])
 );
 GO
 
 CREATE TABLE [ACCollector].[dbo].[BugLocation] (
 	[Location] VARCHAR(15) NOT NULL
-	CONSTRAINT PK_BugLocation PRIMARY KEY CLUSTERED (Location)
+	CONSTRAINT PK_BugLocation PRIMARY KEY CLUSTERED ([Location])
 );
 GO
 
-INSERT INTO [ACCollector].[dbo].[BugLocation](Location) VALUES
+INSERT INTO [ACCollector].[dbo].[BugLocation]([Location]) VALUES
 ('Air'),
 ('Trees'),
 ('Ground'),
@@ -117,11 +170,11 @@ GO
 
 CREATE TABLE [ACCollector].[dbo].[FishLocation] (
 	[Location] VARCHAR(15) NOT NULL
-	CONSTRAINT PK_FishLocation PRIMARY KEY CLUSTERED (Location)
+	CONSTRAINT PK_FishLocation PRIMARY KEY CLUSTERED ([Location])
 );
 GO
 
-INSERT INTO [ACCollector].[dbo].[FishLocation](Location) VALUES
+INSERT INTO [ACCollector].[dbo].[FishLocation]([Location]) VALUES
 ('River'),
 ('River Pool'),
 ('River Mouth'),
@@ -137,10 +190,10 @@ CREATE TABLE [ACCollector].[dbo].[Bug] (
 	[SalePrice] INT NOT NULL,
 	[Location] VARCHAR(15) NOT NULL,
 	[IslandStatus] VARCHAR(10) NOT NULL,
-	CONSTRAINT PK_Bug PRIMARY KEY CLUSTERED (BugId),
-	CONSTRAINT FK_Bug_Game_GameId FOREIGN KEY (GameId) REFERENCES [ACCollector].[dbo].[Game](GameId) ON DELETE CASCADE,
-	CONSTRAINT FK_Bug_BugLocation_Location FOREIGN KEY (Location) REFERENCES [ACCollector].[dbo].[BugLocation](Location),
-	CONSTRAINT FK_Bug_IslandStatus_IslandStatus FOREIGN KEY (IslandStatus) REFERENCES [ACCollector].[dbo].[IslandStatus](IslandStatus),
+	CONSTRAINT PK_Bug PRIMARY KEY CLUSTERED ([BugId]),
+	CONSTRAINT FK_Bug_Game_GameId FOREIGN KEY ([GameId]) REFERENCES [ACCollector].[dbo].[Game]([GameId]) ON DELETE CASCADE,
+	CONSTRAINT FK_Bug_BugLocation_Location FOREIGN KEY ([Location]) REFERENCES [ACCollector].[dbo].[BugLocation]([Location]),
+	CONSTRAINT FK_Bug_IslandStatus_IslandStatus FOREIGN KEY ([IslandStatus]) REFERENCES [ACCollector].[dbo].[IslandStatus]([IslandStatus]),
 	CONSTRAINT CHK_Bug_SalePrice CHECK (SalePrice >= 0)
 );
 GO
@@ -153,11 +206,11 @@ CREATE TABLE [ACCollector].[dbo].[Fish] (
 	[Size] VARCHAR(10) NOT NULL,
 	[Location] VARCHAR(15) NOT NULL,
 	[IslandStatus] VARCHAR(10) NOT NULL,
-	CONSTRAINT PK_Fish PRIMARY KEY CLUSTERED (FishId),
-	CONSTRAINT FK_Fish_Game_GameId FOREIGN KEY (GameId) REFERENCES [ACCollector].[dbo].[Game](GameId) ON DELETE CASCADE,
-	CONSTRAINT FK_Fish_FishSize_Size FOREIGN KEY (Size) REFERENCES [ACCollector].[dbo].[FishSize](Size),
-	CONSTRAINT FK_Fish_FishLocation_Location FOREIGN KEY (Location) REFERENCES [ACCollector].[dbo].[FishLocation](Location),
-	CONSTRAINT FK_Fish_IslandStatus_IslandStatus FOREIGN KEY (IslandStatus) REFERENCES [ACCollector].[dbo].[IslandStatus](IslandStatus),
+	CONSTRAINT PK_Fish PRIMARY KEY CLUSTERED ([FishId]),
+	CONSTRAINT FK_Fish_Game_GameId FOREIGN KEY ([GameId]) REFERENCES [ACCollector].[dbo].[Game]([GameId]) ON DELETE CASCADE,
+	CONSTRAINT FK_Fish_FishSize_Size FOREIGN KEY ([Size]) REFERENCES [ACCollector].[dbo].[FishSize]([Size]),
+	CONSTRAINT FK_Fish_FishLocation_Location FOREIGN KEY ([Location]) REFERENCES [ACCollector].[dbo].[FishLocation]([Location]),
+	CONSTRAINT FK_Fish_IslandStatus_IslandStatus FOREIGN KEY ([IslandStatus]) REFERENCES [ACCollector].[dbo].[IslandStatus]([IslandStatus]),
 	CONSTRAINT CHK_Fish_SalePrice CHECK (SalePrice >= 0)
 );
 GO
@@ -169,11 +222,41 @@ CREATE TABLE [ACCollector].[dbo].[DeepSeaCreature] (
 	[SalePrice] INT NOT NULL,
 	[Size] VARCHAR(10) NOT NULL,
 	[IslandStatus] VARCHAR(10) NOT NULL,
-	CONSTRAINT PK_DeepSeaCreature PRIMARY KEY CLUSTERED (DeepSeaCreatureId),
-	CONSTRAINT FK_DeepSeaCreature_Game_GameId FOREIGN KEY (GameId) REFERENCES [ACCollector].[dbo].[Game](GameId) ON DELETE CASCADE,
-	CONSTRAINT FK_DeepSeaCreature_FishSize_Size FOREIGN KEY (Size) REFERENCES [ACCollector].[dbo].[FishSize](Size),
-	CONSTRAINT FK_DeepSeaCreature_IslandStatus_IslandStatus FOREIGN KEY (IslandStatus) REFERENCES [ACCollector].[dbo].[IslandStatus](IslandStatus),
+	CONSTRAINT PK_DeepSeaCreature PRIMARY KEY CLUSTERED ([DeepSeaCreatureId]),
+	CONSTRAINT FK_DeepSeaCreature_Game_GameId FOREIGN KEY ([GameId]) REFERENCES [ACCollector].[dbo].[Game]([GameId]) ON DELETE CASCADE,
+	CONSTRAINT FK_DeepSeaCreature_FishSize_Size FOREIGN KEY ([Size]) REFERENCES [ACCollector].[dbo].[FishSize]([Size]),
+	CONSTRAINT FK_DeepSeaCreature_IslandStatus_IslandStatus FOREIGN KEY ([IslandStatus]) REFERENCES [ACCollector].[dbo].[IslandStatus]([IslandStatus]),
 	CONSTRAINT CHK_DeepSeaCreature_SalePrice CHECK (SalePrice >= 0)
+);
+GO
+
+CREATE TABLE [ACCollector].[dbo].[Art] (
+	[ArtId] UNIQUEIDENTIFIER NOT NULL CONSTRAINT DF_Art_ArtId DEFAULT NEWSEQUENTIALID(),
+	[GameId] UNIQUEIDENTIFIER NOT NULL,
+	[Name] VARCHAR(40) NOT NULL,
+	[PurchasePrice] INT NOT NULL,
+	[SalePrice] INT NOT NULL,
+	[Type] VARCHAR(10) NOT NULL,
+	[Source] VARCHAR(10) NOT NULL,
+	CONSTRAINT PK_Art PRIMARY KEY CLUSTERED ([ArtId]),
+	CONSTRAINT FK_Art_Game_GameId FOREIGN KEY ([GameId]) REFERENCES [ACCollector].[dbo].[Game]([GameId]) ON DELETE CASCADE,
+	CONSTRAINT FK_Art_ArtType_Type FOREIGN KEY ([Type]) REFERENCES [ACCollector].[dbo].[ArtType]([Type]),
+	CONSTRAINT FK_Art_ArtSource_Source FOREIGN KEY ([Source]) REFERENCES [ACCollector].[dbo].[ArtSource]([Source]),
+	CONSTRAINT CHK_Art_PurchasePrice CHECK (PurchasePrice >= 0),
+	CONSTRAINT CHK_Art_SalePrice CHECK (SalePrice >= 0)
+);
+GO
+
+CREATE TABLE [ACCollector].[dbo].[Fossil] (
+	[FossilId] UNIQUEIDENTIFIER NOT NULL CONSTRAINT DF_Fossil_FossilId DEFAULT NEWSEQUENTIALID(),
+	[GameId] UNIQUEIDENTIFIER NOT NULL,
+	[Name] VARCHAR(40) NOT NULL,
+	[SalePrice] INT NOT NULL,
+	[Group] VARCHAR(20) NOT NULL,
+	CONSTRAINT PK_Fossil PRIMARY KEY CLUSTERED ([FossilId]),
+	CONSTRAINT FK_Fossil_Game_GameId FOREIGN KEY ([GameId]) REFERENCES [ACCollector].[dbo].[Game]([GameId]) ON DELETE CASCADE,
+	CONSTRAINT FK_Fossil_FossilGroup_Group FOREIGN KEY ([Group]) REFERENCES [ACCollector].[dbo].[FossilGroup]([Group]),
+	CONSTRAINT CHK_Fossil_SalePrice CHECK (SalePrice >= 0)
 );
 GO
 
@@ -185,7 +268,7 @@ CREATE TABLE [ACCollector].[dbo].[Availability] (
 	[EndHour] INT NOT NULL,
 	[StartsMidMonth] BIT NOT NULL CONSTRAINT DF_Availability_StartsMidMonth DEFAULT 0,
 	[EndsMidMonth] BIT NOT NULL CONSTRAINT DF_Availability_EndsMidMonth DEFAULT 0
-	CONSTRAINT PK_Availability PRIMARY KEY CLUSTERED (AvailabilityId),
+	CONSTRAINT PK_Availability PRIMARY KEY CLUSTERED ([AvailabilityId]),
 	CONSTRAINT CHK_Availability_StartMonth CHECK (StartMonth >= 1 AND StartMonth <= 12),
 	CONSTRAINT CHK_Availability_StartHour CHECK (StartHour >= 0 AND StartHour <= 23),
 	CONSTRAINT CHK_Availability_EndMonth CHECK (EndMonth >= 1 AND EndMonth <= 12),
@@ -196,60 +279,60 @@ GO
 CREATE TABLE [ACCollector].[dbo].[BugAvailability] (
 	[BugId] UNIQUEIDENTIFIER NOT NULL,
 	[AvailabilityId] UNIQUEIDENTIFIER NOT NULL,
-	CONSTRAINT PK_BugAvailability PRIMARY KEY CLUSTERED (BugId, AvailabilityId),
-	CONSTRAINT FK_BugAvailability_Bug_BugId FOREIGN KEY (BugId) REFERENCES [ACCollector].[dbo].[Bug](BugId) ON DELETE CASCADE,
-	CONSTRAINT FK_BugAvailability_Availability_AvailabilityId FOREIGN KEY (AvailabilityId) REFERENCES [ACCollector].[dbo].[Availability](AvailabilityId) -- TODO: Delete Trigger to clean up Availability
+	CONSTRAINT PK_BugAvailability PRIMARY KEY CLUSTERED ([BugId], [AvailabilityId]),
+	CONSTRAINT FK_BugAvailability_Bug_BugId FOREIGN KEY ([BugId]) REFERENCES [ACCollector].[dbo].[Bug]([BugId]) ON DELETE CASCADE,
+	CONSTRAINT FK_BugAvailability_Availability_AvailabilityId FOREIGN KEY ([AvailabilityId]) REFERENCES [ACCollector].[dbo].[Availability]([AvailabilityId]) -- TODO: Delete Trigger to clean up Availability
 );
 GO
 
 CREATE TABLE [ACCollector].[dbo].[FishAvailability] (
 	[FishId] UNIQUEIDENTIFIER NOT NULL,
 	[AvailabilityId] UNIQUEIDENTIFIER NOT NULL
-	CONSTRAINT PK_FishAvailability PRIMARY KEY CLUSTERED (FishId, AvailabilityId)
-	CONSTRAINT FK_FishAvailability_Fish_FishId FOREIGN KEY (FishId) REFERENCES [ACCollector].[dbo].[Fish](FishId) ON DELETE CASCADE,
-	CONSTRAINT FK_FishAvailability_Availability_AvailabilityId FOREIGN KEY (AvailabilityId) REFERENCES [ACCollector].[dbo].[Availability](AvailabilityId) -- TODO: Delete Trigger to clean up Availability
+	CONSTRAINT PK_FishAvailability PRIMARY KEY CLUSTERED ([FishId], [AvailabilityId])
+	CONSTRAINT FK_FishAvailability_Fish_FishId FOREIGN KEY ([FishId]) REFERENCES [ACCollector].[dbo].[Fish]([FishId]) ON DELETE CASCADE,
+	CONSTRAINT FK_FishAvailability_Availability_AvailabilityId FOREIGN KEY ([AvailabilityId]) REFERENCES [ACCollector].[dbo].[Availability]([AvailabilityId]) -- TODO: Delete Trigger to clean up Availability
 );
 GO
 
 CREATE TABLE [ACCollector].[dbo].[DeepSeaCreatureAvailability] (
 	[DeepSeaCreatureId] UNIQUEIDENTIFIER NOT NULL,
 	[AvailabilityId] UNIQUEIDENTIFIER NOT NULL
-	CONSTRAINT PK_DeepSeaCreatureAvailability PRIMARY KEY CLUSTERED (DeepSeaCreatureId, AvailabilityId)
-	CONSTRAINT FK_DeepSeaCreatureAvailability_DeepSeaCreature_DeepSeaCreatureId FOREIGN KEY (DeepSeaCreatureId) REFERENCES [ACCollector].[dbo].[DeepSeaCreature](DeepSeaCreatureId) ON DELETE CASCADE,
-	CONSTRAINT FK_DeepSeaCreatureAvailability_Availability_AvailabilityId FOREIGN KEY (AvailabilityId) REFERENCES [ACCollector].[dbo].[Availability](AvailabilityId) -- TODO: Delete Trigger to clean up Availability
+	CONSTRAINT PK_DeepSeaCreatureAvailability PRIMARY KEY CLUSTERED ([DeepSeaCreatureId], [AvailabilityId])
+	CONSTRAINT FK_DeepSeaCreatureAvailability_DeepSeaCreature_DeepSeaCreatureId FOREIGN KEY ([DeepSeaCreatureId]) REFERENCES [ACCollector].[dbo].[DeepSeaCreature]([DeepSeaCreatureId]) ON DELETE CASCADE,
+	CONSTRAINT FK_DeepSeaCreatureAvailability_Availability_AvailabilityId FOREIGN KEY ([AvailabilityId]) REFERENCES [ACCollector].[dbo].[Availability]([AvailabilityId]) -- TODO: Delete Trigger to clean up Availability
 );
 GO
 
 CREATE TABLE [ACCollector].[dbo].[Note] (
 	[NoteId] UNIQUEIDENTIFIER NOT NULL CONSTRAINT DF_Note_NoteId DEFAULT NEWSEQUENTIALID(),
 	[Message] VARCHAR(255) NOT NULL,
-	CONSTRAINT PK_Note PRIMARY KEY CLUSTERED (NoteId)
+	CONSTRAINT PK_Note PRIMARY KEY CLUSTERED ([NoteId])
 );
 GO
 
 CREATE TABLE [ACCollector].[dbo].[BugNote] (
 	[BugId] UNIQUEIDENTIFIER NOT NULL,
 	[NoteId] UNIQUEIDENTIFIER NOT NULL
-	CONSTRAINT PK_BugNote PRIMARY KEY CLUSTERED (BugId, NoteId),
-	CONSTRAINT FK_BugNote_Bug_BugId FOREIGN KEY (BugId) REFERENCES [ACCollector].[dbo].[Bug](BugId) ON DELETE CASCADE,
-	CONSTRAINT FK_BugNote_Note_NoteId FOREIGN KEY (NoteId) REFERENCES [ACCollector].[dbo].[Note](NoteId) -- TODO: Delete Trigger to clean up Note
+	CONSTRAINT PK_BugNote PRIMARY KEY CLUSTERED ([BugId], [NoteId]),
+	CONSTRAINT FK_BugNote_Bug_BugId FOREIGN KEY ([BugId]) REFERENCES [ACCollector].[dbo].[Bug]([BugId]) ON DELETE CASCADE,
+	CONSTRAINT FK_BugNote_Note_NoteId FOREIGN KEY ([NoteId]) REFERENCES [ACCollector].[dbo].[Note]([NoteId]) -- TODO: Delete Trigger to clean up Note
 );
 GO
 
 CREATE TABLE [ACCollector].[dbo].[FishNote] (
 	[FishId] UNIQUEIDENTIFIER NOT NULL,
 	[NoteId] UNIQUEIDENTIFIER NOT NULL
-	CONSTRAINT PK_FishNote PRIMARY KEY CLUSTERED (FishId, NoteId),
-	CONSTRAINT FK_FishNote_Fish_FishId FOREIGN KEY (FishId) REFERENCES [ACCollector].[dbo].[Fish](FishId) ON DELETE CASCADE,
-	CONSTRAINT FK_FishNote_Note_NoteId FOREIGN KEY (NoteId) REFERENCES [ACCollector].[dbo].[Note](NoteId) -- TODO: Delete Trigger to clean up Note
+	CONSTRAINT PK_FishNote PRIMARY KEY CLUSTERED ([FishId], [NoteId]),
+	CONSTRAINT FK_FishNote_Fish_FishId FOREIGN KEY ([FishId]) REFERENCES [ACCollector].[dbo].[Fish]([FishId]) ON DELETE CASCADE,
+	CONSTRAINT FK_FishNote_Note_NoteId FOREIGN KEY ([NoteId]) REFERENCES [ACCollector].[dbo].[Note]([NoteId]) -- TODO: Delete Trigger to clean up Note
 );
 GO
 
 CREATE TABLE [ACCollector].[dbo].[DeepSeaCreatureNote] (
 	[DeepSeaCreatureId] UNIQUEIDENTIFIER NOT NULL,
 	[NoteId] UNIQUEIDENTIFIER NOT NULL
-	CONSTRAINT PK_DeepSeaCreatureNote PRIMARY KEY CLUSTERED (DeepSeaCreatureId, NoteId),
-	CONSTRAINT FK_DeepSeaCreatureNote_DeepSeaCreature_DeepSeaCreatureId FOREIGN KEY (DeepSeaCreatureId) REFERENCES [ACCollector].[dbo].[DeepSeaCreature](DeepSeaCreatureId) ON DELETE CASCADE,
-	CONSTRAINT FK_DeepSeaCreatureNote_Note_NoteId FOREIGN KEY (NoteId) REFERENCES [ACCollector].[dbo].[Note](NoteId) -- TODO: Delete Trigger to clean up Note
+	CONSTRAINT PK_DeepSeaCreatureNote PRIMARY KEY CLUSTERED ([DeepSeaCreatureId], [NoteId]),
+	CONSTRAINT FK_DeepSeaCreatureNote_DeepSeaCreature_DeepSeaCreatureId FOREIGN KEY ([DeepSeaCreatureId]) REFERENCES [ACCollector].[dbo].[DeepSeaCreature]([DeepSeaCreatureId]) ON DELETE CASCADE,
+	CONSTRAINT FK_DeepSeaCreatureNote_Note_NoteId FOREIGN KEY ([NoteId]) REFERENCES [ACCollector].[dbo].[Note]([NoteId]) -- TODO: Delete Trigger to clean up Note
 );
 GO
